9c94682be7673281a4d7b31c9e6305c9a0ac0f0c
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-23 00:01:16 +0530

    mern

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors =require('cors')
 const mongoose = require ('mongoose')
+const User = require('./models/user.model')
 
 app.use(cors())
 app.use(express.json())  //telling express that anything coming to body should be passed on as JSON.
@@ -24,17 +25,18 @@ app.post('/api/register', async (req, res) => {
 })
 
 app.post('/api/login', async (req, res) => {
-    console.log(req,body)
-    try {
-        await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        })
-        res.json({ status: 'ok' })
-    } catch (err) {
-        res.json({ status: 'error', error: 'Duplicate email' })
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    if (user) {
+        return res.json({ status: 'ok', user: true})
+    } else {
+        return res.json({ status: 'error', user: false})
     }
+
+ 
    
 })
 
